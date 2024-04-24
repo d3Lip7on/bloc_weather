ab4b6a5534d8607279980c02db8c671879d8cc05
Vladislav Vagner <vladislawwagner@gmail.com>
2024-04-24 21:31:12 +0200

    added open_meteo_api package with already defined models location and weather

---
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
+class Weather {
+  final double temperature;
+  @JsonKey(name: 'weathercode')
+  final double weatherCode;
+
+  const Weather({required this.temperature, required this.weatherCode});
+
+  Map<String, dynamic> toJson() => _$WeatherToJson(this);
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+}
