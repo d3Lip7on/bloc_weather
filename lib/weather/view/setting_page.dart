a5e0a5a737d861df26b05eb9c8984425d0a67de4
Vladislav Vagner <vladislawwagner@gmail.com>
2024-05-13 21:42:01 +0200

    added settings page

---
+++ lib/weather/view/setting_page.dart
@@ -0,0 +1,48 @@
+import 'dart:ffi';
+
+import 'package:bloc_wheather/weather/cubit/weather_cubit.dart';
+import 'package:bloc_wheather/weather/cubit/weather_state.dart';
+import 'package:bloc_wheather/weather/models/models.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage._();
+
+  static Route<void> route(WeatherCubit weatherCubit) {
+    return MaterialPageRoute(
+        builder: (_) => BlocProvider.value(
+              value: weatherCubit,
+              child: const SettingsPage._(),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          BlocBuilder<WeatherCubit, WeatherState>(
+              buildWhen: (previous, current) =>
+                  previous.temperatureUnits != current.temperatureUnits,
+              builder: (context, state) {
+                return ListTile(
+                  title: const Text('Temperature units'),
+                  isThreeLine: true,
+                  subtitle: const Text('Use metric measurements for '
+                      'temperature units.'),
+                  trailing: Switch(
+                    value: state.temperatureUnits.isCelsius,
+                    onChanged: (_) =>
+                        context.read<WeatherCubit>().toggleUnits(),
+                  ),
+                );
+              })
+        ],
+      ),
+    );
+  }
+}
