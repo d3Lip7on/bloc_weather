528fe2966d9b3b865195adda92eabe08bde18c63
Vladislav Vagner <vladislawwagner@gmail.com>
2024-05-14 10:17:04 +0200

    added weather app file

--- lib/app/weather_app.dart
+++ lib/app/weather_app.dart
@@ -1,18 +1,48 @@
+import 'package:bloc_wheather/theme/cubit/theme_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_repository/weather_repository.dart';
 
 import '../weather/view/weather_page.dart';
 
 class WeatherApp extends StatelessWidget {
-  const WeatherApp({super.key});
+  WeatherApp({super.key, required WeatherRepository weatherRepository})
+      : _weatherRepository = weatherRepository;
+
+  final WeatherRepository _weatherRepository;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider.value(
+      value: _weatherRepository,
+      child: BlocProvider(
+        create: (_) => ThemeCubit(),
+        child: const WeatherAppView(),
       ),
-      home: WeatherPage(),
+    );
+  }
+}
+
+class WeatherAppView extends StatelessWidget {
+  const WeatherAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, Color>(
+      builder: (context, color) {
+        return MaterialApp(
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+            colorScheme: ColorScheme.fromSeed(seedColor: color),
+            textTheme: GoogleFonts.rajdhaniTextTheme(),
+          ),
+          home: const WeatherPage(),
+        );
+      },
     );
   }
 }
