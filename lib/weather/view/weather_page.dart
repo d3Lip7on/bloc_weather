f5fb60df135c9e1ec8eafdadf42e8a3b02836493
Vladislav Vagner <vladislawwagner@gmail.com>
2024-05-14 09:58:13 +0200

    added barrel file widgets.dart

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_repository/weather_repository.dart';
 
-import '../widgets/weather_empty.dart';
+import '../widgets/widgets.dart';
 
 class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
